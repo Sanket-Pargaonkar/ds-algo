1a04da5a0e0ba1a74d6ca2a3e2055bfe2529bfb7
Sanket <sanket.pargaonkar@gmail.com>
2020-09-17 22:46:01 +0530

    WIP simple linked list

--- ds/linkedlist/src/main/java/ed/dsalgo/linkedlist/App.java
+++ ds/linkedlist/src/main/java/ed/dsalgo/linkedlist/App.java
@@ -1,7 +1,16 @@
 package ed.dsalgo.linkedlist;
 
+import ed.dsalgo.linkedlist.impl.SimpleLinkedList;
+
 public class App{
     public static void main(String[] args){
 	System.out.println("Hello world");
+
+	SimpleLinkedList sll = new SimpleLinkedList();
+	sll.add(3);
+	sll.add(4);
+	sll.add(5);	
+	System.out.println(sll.toString());
+	System.out.println(sll.size());	
     }
 }
--- ds/linkedlist/src/main/java/ed/dsalgo/linkedlist/impl/SimpleLinkedList.java
+++ ds/linkedlist/src/main/java/ed/dsalgo/linkedlist/impl/SimpleLinkedList.java
@@ -1,5 +1,87 @@
 package ed.dsalgo.linkedlist.impl;
 
-public class SimpleLinkedList{
+import java.lang.StringBuilder;
 
+public class SimpleLinkedList{
+    public Node head;
+    public Node tail;
+    public SimpleLinkedList(){
+
+    }
+
+    public SimpleLinkedList(int val){
+	Node head = new Node(val, null);
+	this.head = head;
+	tail = head;	
+    }
+
+    ///traverse list:
+    ///print list
+    @Override
+    public String toString(){
+	StringBuilder sb = new StringBuilder();
+	if(head != null){
+	    Node curr = head;
+	    while(curr != null){
+	        sb.append(String.valueOf(curr.value));
+		if(curr.next != null )
+		{
+		    sb.append(", ");
+		}
+		curr = curr.next;		
+	    }
+	}
+	return sb.toString();
+    }
+    ///size of list
+    public int size(){
+	if(head == null)
+	    return 0;
+	int count = 0;
+	Node curr = head;
+	 
+	while(curr != null) {
+	    count++;
+	    curr = curr.next;
+	}
+       return count;
+    }
+    
+    ///add nodes
+    public void add(int val){
+	Node _new = new Node(val, null);
+	if(head == null){
+	    head = _new;
+	}
+	else if(head.next == null){
+	    head.next = _new;
+	}
+	else 
+	    tail.next = _new;
+	tail = _new;
+    }
+    //add node at
+    public void add(int val, int pos){
+	///if list is empty simpoly call add()
+	//if pos < size() throw exceptoon
+	//
+	
+    }
+    //add first
+    //add lst
+    
+    ///remove nodes
+    //remove node at    
+    //remove first
+    //remove lst
+    
+}
+
+class Node{
+    public Node(int value, Node next){
+	this.value = value;
+	this.next = next;
+    }
+    public int value;
+    public Node next;
 }
