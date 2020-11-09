142f9c6f094f47b1aa578f1b9bf1c98f64dbaa0e
Sanket <sanket.pargaonkar@gmail.com>
2020-11-09 23:42:22 +0530

    Adds project scaffolding for stacks-ds with tests

---
+++ ds/stacks/src/main/java/ed/dsalgo/stacks/StackDemo.java
@@ -0,0 +1,14 @@
+/**
+ *@author Sanket Pargaonakar
+ *@version 1.0
+ *@since  
+ */
+
+package ed.dsalgo.stacks;
+import static java.lang.System.out;
+
+public class StackDemo{
+    public static void main(String[] args){
+	out.println("hello");
+    }
+}
---
+++ ds/stacks/src/test/java/ed/dsalgo/stacks/TestStackDemo.java
@@ -0,0 +1,19 @@
+/**
+ *@author Sanket Pargaonakar
+ *@version 1.0
+ *@since  
+ */
+
+package ed.dsalgo.stacks;
+import static java.lang.System.out;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+    
+
+public class TestStackDemo{
+    @Test
+    public void test1(){
+	// assertEquals(StackDemo.main(null), "hello");
+	StackDemo.main(null);
+    }
+}
