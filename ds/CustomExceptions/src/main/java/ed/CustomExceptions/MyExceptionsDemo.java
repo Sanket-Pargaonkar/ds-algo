6962f4be3168dae3b1a6e10291965fdb33808402
Sanket <sanket.pargaonkar@gmail.com>
2020-11-11 13:42:32 +0000

    Adds Customexceptions library to the repo

---
+++ ds/CustomExceptions/src/main/java/ed/CustomExceptions/MyExceptionsDemo.java
@@ -0,0 +1,7 @@
+package ed.CustomExceptions;
+
+public class MyExceptionsDemo{
+    public static void main(String[] args){
+	System.out.println("This jar file contains custom exceptions used in the ds-algo code");
+    }
+}
---
+++ ds/CustomExceptions/src/main/java/ed/CustomExceptions/MyStackEmptyException.java
@@ -0,0 +1,7 @@
+package ed.CustomExceptions;
+
+public class MyStackEmptyException extends Exception{
+    public MyStackEmptyException(){
+	super();
+    }
+}
