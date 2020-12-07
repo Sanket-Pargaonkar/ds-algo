5b4910d4c6e03fa3981a49331eb5bf07efb387b2
Sanket <sanket.pargaonkar@gmail.com>
2020-12-07 16:22:33 +0000

    Adds Test Scaffolding for PatterMatcher132 class for 132 pattern matching problem

---
+++ ds/stacks/src/main/java/ed/dsalgo/stacks/PatternMatcher132.java
@@ -0,0 +1,18 @@
+package  ed.dsalgo.stacks
+
+/**
+ *@author Sanket Pargaonkar
+ *@version 1.0
+ *@since 07-12-2020
+ */;
+
+import static java.lang.System.out;
+
+public class PatternMatcher132{
+
+    public PatternMatcher132(){
+
+    }
+
+    
+}
---
+++ ds/stacks/src/test/java/ed/dsalgo/stacks/TestPatternMatcher132.java
@@ -0,0 +1,23 @@
+package ed.dsalgo.stacks;
+
+/**
+ *@atuthor Sanke Parganokar
+ *@version 1.0
+ *@since 07-12-2020
+ */
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import static org.junit.jupiter.Assertions.assertAll;
+import org.junit.jupiter.api.Test;
+
+public class TestPatternMatcher132{
+
+    @Test
+    public void initTest(){
+	PatternMatcher132 pm132 = new PatternMatcher132();
+	assertNotNull(pm132);
+    }
+    
+}
+
