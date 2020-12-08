e85281b672a1ec08f994e29f4089d109caa0c530
Sanket <sanket.pargaonkar@gmail.com>
2020-12-08 11:34:40 +0000

    Adds o(n^2) solution to the 132 pattern matcher problem

--- ds/stacks/src/main/java/ed/dsalgo/stacks/PatternMatcher132.java
+++ ds/stacks/src/main/java/ed/dsalgo/stacks/PatternMatcher132.java
@@ -10,9 +10,40 @@ import static java.lang.System.out;
 
 public class PatternMatcher132{
 
-    public PatternMatcher132(){
+    private final int[] input;
 
+    public PatternMatcher132(){
+	this.input = null;
     }
 
+    public PatternMatcher132(int... input){
+	this.input = input;
+    }
+
+    /**
+     * This is the O(n^2)implementation of the solution
+     */
+    public int[] Get132Pattern1(){
+	int[] result = new int[3];
+	boolean patternFound = false;	
+	for(int i : input){
+	    int ele = i;
+	    result[0] = ele;
+	    boolean secondStep = false;	    
+	    for(int j : input){
+		if(!secondStep && ele < j){
+		    result[2] = j;
+		    secondStep = true;
+		    continue;
+		}else if(secondStep &&  (result[0]<j && j> result[2])){
+		    result[1] = j;
+		    patternFound = true;
+		    break;
+		}
+	    }
+	    if(patternFound)break;
+	}
+	return patternFound?result:null;
+    }
     
 }
--- ds/stacks/src/test/java/ed/dsalgo/stacks/TestPatternMatcher132.java
+++ ds/stacks/src/test/java/ed/dsalgo/stacks/TestPatternMatcher132.java
@@ -13,10 +13,21 @@ import org.junit.jupiter.api.Test;
 
 public class TestPatternMatcher132{
 
+    private PatternMatcher132 pm132;
+    
     @Test
     public void initTest(){
 	PatternMatcher132 pm132 = new PatternMatcher132();
 	assertNotNull(pm132);
+    }
+
+    @Test
+    public void testGet132Pattern1(){
+	final int[] input = {2,3,5,4,6,5,67,454,54,6};
+	this.pm132 = new PatternMatcher132(input);
+	int[] result = pm132.Get132Pattern1();
+	assertNotNull(result);
+	for(int i : result)out.println(i);
     }
     
 }
