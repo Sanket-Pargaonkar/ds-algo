252064581d85bbd278169d07038052a5b611a5c5
sanket.pargoankar <sanket.pargoankar@gmail.com>
2024-09-17 01:46:44 +0530

    Adds dynamic array code. Sorting is yet to be inmplemnted.

--- ds/arrays/src/main/java/ed/dsalgo/arrays/Demo.java
+++ ds/arrays/src/main/java/ed/dsalgo/arrays/Demo.java
@@ -1,11 +1,31 @@
 package ed.dsalgo.arrays;
 
 import ed.dsalgo.arrays.*;
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 
+public class Demo {
+	public static void main(String[] args) {
+		DynamicArray da = new DynamicArray(10);
+		try {
+			System.out.println("Inserting 3 elements");
+			da.insert(0, 1);
+			da.insert(1, 2);
+			da.insert(2, 3);
+			da.traverse();
+			System.out.println("Traversing 3  elements");
+			da.traverse(0, 2);
+			System.out.println("Inserting 1 element that is out of bound");
+			da.insert(10, 11);
+			da.traverse();
+			System.out.println("Reading 11th  element");
+			System.out.println(da.read(10));
+			System.out.println("Reading 11th  element: 11");
+			System.out.println(da.search(10));
+			System.out.println("Reading element: 7");
+			System.out.println(da.search(7));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-public class Demo{
-    public static void main(String[] args) {
-        DynamicArray da = new DynamicArray(10);
-	
-    }
+	}
 }
--- ds/arrays/src/main/java/ed/dsalgo/arrays/DynamicArray.java
+++ ds/arrays/src/main/java/ed/dsalgo/arrays/DynamicArray.java
@@ -1,27 +1,132 @@
 package ed.dsalgo.arrays;
+
 import ed.CustomExceptions.WrongInsertPositionException;
+import java.lang.System;
+import java.util.NoSuchElementException;
 
-public class DynamicArray{
-    private int size;
-    private int capacity;
-    private int[] array;
-    public DynamicArray(int capacity) {
-        this.capacity = capacity;    
-        this.array = new int[capacity];
-    }
+public class DynamicArray {
+	private int size;
+	private int capacity;
+	private static final int OFFSET = 5;
+	public static final int DEFAULT_CAPACITY = 5;
+	private int[] array;
 
-    //traverse 
-    public void traverse(){
-	for(){}
+	public DynamicArray(int capacity) {
+		this.capacity = capacity;
+		this.array = new int[capacity];
+	}
 
-    }
-    //insert
-    public boolean insert(int i, int pos){
+	public DynamicArray() {
+		this.capacity = DEFAULT_CAPACITY;
+		this.array = new int[capacity];
+	}
 
-	return false;
-    }
-    //delete
-    //sort
-    //search
+	// traverse
+	public void traverse() {
+		System.out.println("\nPrinting all the " + this.array.length + " elements of the \n");
+		for (int i = 0; i < this.array.length; i++) {
+			System.out.print(this.array[i]);
+			if (i < this.array.length - 1)
+				System.out.print(",");
+		}
+		System.out.print("\n");
+	}
+
+	public void traverse(int start, int end) {
+		if (start < 0) {
+			throw new IllegalArgumentException("Start position in the array cannot be less than 0");
+		}
+		if (end > this.array.length - 1) {
+			throw new IllegalArgumentException(
+					"End position in the array cannot be greater than  length of the array:"
+							+ this.array.length + " 0");
+		}
+
+		System.out.println("\nPrinting all the " + this.array.length + " elements of the ");
+		for (int i = start; i <= end; i++) {
+			System.out.print(this.array[i]);
+			if (i < end)
+				System.out.print(",");
+		}
+		System.out.print("\n");
+
+	}
+
+	// insert
+	public void insert(int pos, int i) {
+
+		if (pos < 0) {
+			throw new IllegalArgumentException(
+					"insert position in the array should be greater than or equal to 0");
+		}
+
+		if (pos > this.array.length - 1) {
+			System.out.println("\n");
+			System.out.println("Array capacity reached. Increasing capacity by default inc_offset: " + OFFSET);
+			System.out.println("\n");
+			int[] temp = new int[this.array.length + OFFSET];
+			for (int j = 0; j < this.array.length; j++)
+				temp[j] = this.array[j];
+			this.array = temp;
+		}
+
+		this.array[pos] = i;
+	}
+
+	// read
+	public int read(int pos) {
+		System.out.println("\n");
+		if (pos < 0) {
+			throw new IllegalArgumentException(
+					"insert position in the array should be greater than or equal to 0");
+		}
+		if (pos > this.array.length - 1) {
+			throw new IllegalArgumentException(
+					"insert position in the array should be less than the length of the array : " + this.array.length);
+		}
+		return this.array[pos];
+	}
+
+	// update
+	public void update(int pos, int val) {
+		System.out.println("\n");
+		if (pos < 0) {
+			throw new IllegalArgumentException(
+					"update position in the array should be greater than or equal to 0");
+		}
+		if (pos > this.array.length - 1) {
+			throw new IllegalArgumentException(
+					"update position in the array should be less than the length of the array : " + this.array.length);
+		}
+		this.array[pos] = val;
+	}
+
+	// delete
+	public void delete(int pos) {
+		System.out.println("\n");
+		if (pos < 0) {
+			throw new IllegalArgumentException(
+					"delete position in the array should be greater than or equal to 0");
+		}
+		if (pos > this.array.length - 1) {
+			throw new IllegalArgumentException(
+					"delete position in the array should be less than the length of the array : " + this.array.length);
+		}
+		this.array[pos] = 0;
+	}
+
+	// sort
+	public void sort() {
+
+	}
+
+	// search
+	public int search(int val) {
+		for (int i = 0; i < this.array.length; i++) {
+			if (this.array[i] == val)
+				return i;
+		}
+		throw new NoSuchElementException(val + " not found in the array");
+
+	}
 }
-
